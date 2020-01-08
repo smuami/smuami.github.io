32376e702172b418b16e8bf8b42466069b7c29cd
Ismael Robles <iarobles271@gmail.com>
2020-01-08 17:25:08 -0600

    revisión de estilos

--- recursos/js/principal.js
+++ recursos/js/principal.js
@@ -1,22 +1,9 @@
-//general
-$(function(){
-    $("#menu li a").click(function(evento) {
-       var enlace = $(evento.target);
-       var seccion = $(enlace.attr("href"));
-       if(enlace.length){
-
-       $('html, body').animate({
-          scrollTop: seccion.offset().top
-       }, 1000);
-
-       }
-    });
-});
 
 //calendario
  $(function() {
-
+        var cajaPrincipal = $('html, body');
         var calendario = $(".calendario");
+        var seccionCalendario = $("#calendario");
         var resumenCharla = null;
         
         //$(".calendario thead .hidden").removeClass("hidden");
@@ -38,10 +25,39 @@ $(function(){
             evento.preventDefault();
 
             resumenCharla.fadeOut("fast", function() {
-                calendario.fadeIn();
+                calendario.fadeIn(function(){
+                    cajaPrincipal.animate({
+                        scrollTop: seccionCalendario.offset().top
+                    }, 1000);
+                });
             });
 
             return false;
         });
 
+});
+
+ //general
+$(function(){    
+    var cajaPrincipal = $('html, body');
+
+    //scroll en click de secciones
+    $("#menu li a").click(function(evento) {
+       var enlace = $(evento.target);
+       var seccion = $(enlace.attr("href"));
+       if(enlace.length){
+
+       cajaPrincipal.animate({
+          scrollTop: seccion.offset().top
+       }, 1000);
+
+       }
+    });        
+});
+
+//ARRANCAR APLICACION
+//$(window).load(function(){alert(1);});
+$( window ).on("load", function() {
+  // Run code  
+  $("#caja-loading").fadeOut("fast");
 });
