336576d025d4c1ebf2c44aa8d7e63b3f0e08a203
Ismael Robles <iarobles271@gmail.com>
2020-01-08 22:17:27 -0600

    primera versión estable con diseño responsivo

--- recursos/js/principal.js
+++ recursos/js/principal.js
@@ -40,19 +40,43 @@
  //general
 $(function(){    
     var cajaPrincipal = $('html, body');
+    var menu = $("#menu ul");
+    var menuAbierto = false;
+    var botonResponsivo = $("#boton-menu-responsive");
+
+    var enbotonPresionado = function(){
+        if(botonResponsivo.text()==="☰"){
+            botonResponsivo.text("X");
+            menuAbierto=true;
+        } else {
+            botonResponsivo.text("☰");
+            menuAbierto=false;
+        }
+        menu.toggleClass("menu-oculto-en-mediaquery");
+    };
 
     //scroll en click de secciones
     $("#menu li a").click(function(evento) {
        var enlace = $(evento.target);
        var seccion = $(enlace.attr("href"));
-       if(enlace.length){
+       if(enlace.length){//si la caja existe
+           $(".seleccionado").removeClass("seleccionado");
+           enlace.parent().addClass("seleccionado");
 
-       cajaPrincipal.animate({
-          scrollTop: seccion.offset().top
-       }, 1000);
+           if(menuAbierto){
+             enbotonPresionado();
+             menuAbierto=false;
+           }
+
+           //scroll hacia la caja
+           cajaPrincipal.animate({
+              scrollTop: seccion.offset().top
+           }, 1000);
 
        }
-    });        
+    });    
+
+    botonResponsivo.click(enbotonPresionado);    
 });
 
 //ARRANCAR APLICACION
